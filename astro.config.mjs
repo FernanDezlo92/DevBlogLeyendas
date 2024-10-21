37e2c751d4e70f2065fe6075170e8676737a74cb
FernanDezlo <fernanfernandezag@gmail.com>
2024-10-21 17:07:43 +0200

    Added button buy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField} from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // output: 'server',
+  integrations: [tailwind()],
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean( {context:'server', access: 'public'}),
+    }
+  },
 });
